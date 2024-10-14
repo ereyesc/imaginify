d7d9bc5ef433c890593d3bd7646acd3184ddda98
Erick <erick.erc.10@hotmail.com>
2024-10-14 15:06:25 -0600

    final changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { IBM_Plex_Sans } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 
 const IBMPlex = IBM_Plex_Sans({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <html lang='en'>
         <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
           {children}
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
